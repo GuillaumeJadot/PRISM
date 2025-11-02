2d9c15945c6bbf7be47dccac75b4fd2c2cde8849
Guillaume Jadot <guillaumejadot@Guillaumes-MacBook-Air.local>
2025-11-02 17:59:39 +0100

    update html - health effects and action buttons

--- app.py
+++ app.py
@@ -17,11 +17,11 @@ from folium.plugins import MousePosition
 from jinja2 import Template
 
 app = Flask(__name__)
-base_dir = r"/srv/data/Variables"
+# base_dir = r"/srv/data/Variables"
 # base_dir =r"/export/store09/backup_web/web/maps_server/prism/"
 
 #base_dir = r"/export/homes/students/gujadot/website/Variables"
-# base_dir = "/Users/guillaumejadot/Library/CloudStorage/OneDrive-UCL/University/Master2/Q1/LBIRE2234/Variables/"
+base_dir = "/Users/guillaumejadot/Library/CloudStorage/OneDrive-UCL/University/Master2/Q1/LBIRE2234/Variables/"
 
 # wallon_layer = os.path.join(base_dir, r"Wallonie.shp")
 
